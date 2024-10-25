71913ef8631820434707daf026ae7ec3563bdb21
ralucaachitei1207 <me_raluk93@hotmail.it>
2024-10-25 21:53:49 +0100

    Added Project2 Functions folder

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "I am learning";  // Initial sentence
+    sentence += " a lot from this book!";  // Concatenating another part to the sentence
+    document.getElementById("Concatenate").innerHTML = sentence;  // Displaying the concatenated sentence
+}
