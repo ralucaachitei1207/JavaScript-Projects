b5e20625f1cabf9011789baf45cbb1ccc254a372
ralucaachitei1207 <me_raluk93@hotmail.it>
2024-10-25 13:50:36 +0100

    Added new folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+//window.alert method 
+window.alert("Hello, world");
+
+//document.write method
+document.write("welcome to my page!")
+
+//creating two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+//an expression
+document.write(67 * 77);
